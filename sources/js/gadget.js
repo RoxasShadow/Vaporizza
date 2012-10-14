29746d2df1d7dc50617d086559d7e112114b7330
Giovanni Capuano <webmaster@giovannicapuano.net>
2012-10-14 14:29:41 +0200

    Support at non-Steam games added.

--- sources/js/gadget.js
+++ sources/js/gadget.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 by Giovanni Capuano <webmaster@giovannicapuano.net>
-
+ *
  * Vaporizza is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -14,163 +14,57 @@
  * You should have received a copy of the GNU General Public License
  * along with Vaporizza.  If not, see <http://www.gnu.org/licenses/>.
  */
-function Steam(fso, shell) {
-	var steam = this;
+function CheckDockState() {
+  var height        = (screen.height - 50) / 2;
+  var scaleDocked   = 1;
+  var scaleUndocked = 2;
 
-	this.getConfig = function(path, file) {
-		var folder = fso.GetFolder(path);
-		var sub    = new Enumerator(folder.SubFolders);
-
-		if (sub.atEnd())
-			return false;
-    
-    return fso.OpenTextFile(sub.item() + file).ReadAll().replace(/"[\t ]+"/gm, "\" : \"").replace(/"([\t\r\n ]+\{)/g, "\":$1").replace(/([}"])([\t\r\n ]+\")/g, "$1,$2");
-	}
-  
-  this.getUserID = function(path, file) {
-    return fso.OpenTextFile(path + file).ReadAll();
-  }
-
-	this.loadGames = function(gamelist, gameData, userData) {
-		var games;
-
-		if (gameData != false) {
-			games = eval('({' + gameData + '})');
-			games = (games.UserLocalConfigStore || games.UserRoamingConfigStore);
-			games = games.Software.Valve.Steam.apps;
-			for (var id in games)
-        gamelist.push({
-          id          : id,
-          name        : '',
-          lastplayed  : games[id].lastplayed,
-          tags        : games[id].tags
-        });
-		}
-	}
-    
-	this.convalidateGames = function(gamelist, userid, imagesize) {
-		var filteredGamelist = [];
-
-		$.ajax({
-			url       : 'http://steamcommunity.com/id/' + userid + '/games?tab=all&xml=1',
-			type      : 'get',
-			dataType  : 'xml',
-			async     : false,
-			success   : function(data) {
-
-				var allowedGames = [];
-				$('game', data).each(function() {
-					id   = $('appID', this).text();
-					name = $('name', this).text();
-
-					allowedGames.push(id);
-
-					$.each(gamelist, function(i, game) {
-						if (game.id == id)
-							game.name = name;
-					});
-				});
-
-				$.each(gamelist, function(i, game) {
-					if ($.inArray(game.id, allowedGames) != -1)
-						filteredGamelist.push(game);
-				});
-
-			}
-		});
-
-		filteredGamelist.sort(steam.compare);
-		return filteredGamelist;
-	}
-
-	this.getInstallationPath = function() {
-		try {
-			return shell.RegRead('HKLM\\Software\\Wow6432Node\\Valve\\Steam\\InstallPath');
-		} catch(e) {
-			return shell.RegRead('HKLM\\Software\\Valve\\Steam\\InstallPath');
-		}
-	}
-
-	this.compare = function(a, b) {
-		return ((a.lastplayed < b.lastplayed) ? 1 : ((a.lastplayed > b.lastplayed) ? -1 : 0));
-	}
-
-	this.printGameList = function(gamelist, imagesize) {
-    var length = gamelist.length;
-		$('#gamelist').text('');
-
-		var image;
-		if (imagesize == 'small') {
-			image = 'capsule_231x87.jpg';
-			$('body').css('width', '251px');
-		} else if (imagesize == 'medium') {
-			image = 'header_292x136.jpg';
-			$('body').css('width', '312px');
-		} else {
-			image = 'header.jpg';
-			$('body').css('width', '480px');
-		}
-
-		for (var i = 0; i < length; ++i)
-			$('#gamelist').append('<li class="game"><a href="steam://rungameid/' + gamelist[i].id + '"><img src="http://cdn.steampowered.com/v/gfx/apps/' + gamelist[i].id + '/' + image + '" /></a></li>');
-	}
-  
-  this.getSettings = function(what) {
-    if(what == 'imagesize') {
-      var imagesize = System.Gadget.Settings.readString('imagesize');
-      if(imagesize == '' || (imagesize != 'small' && imagesize != 'medium' && imagesize != 'large'))
-        return 'small';
-      else
-        return imagesize;
-    }
-    else
-      return System.Gadget.Settings.readString(what);
-  }
-  
+  $('body').css('height', height * (System.Gadget.docked ? scaleDocked : scaleUndocked));
 }
 
-function checkDockState() {
-  var height         = (screen.height - 50) / 2;
-  var scaleDocked    = 1;
-  var scaleUndocked  = 2;
-  var timeTransition = 2;
-  var oBody          = document.body.style;
-  
-  if (System.Gadget.docked)
-      $('body').css('height', height * scaleDocked);
-  else
-      $('body').css('height', height * scaleUndocked);
+function SettingsClosed(event) {
+  if(event.closeAction == event.Action.commit)
+    init();
 }
 
 function init() {
-	try {
-		var fso   = new ActiveXObject('Scripting.FileSystemObject');
-		var shell = new ActiveXObject('WScript.Shell');
-		var steam = new Steam(fso, shell);
+  try {
+    var fso       = new ActiveXObject('Scripting.FileSystemObject');
+    var shell     = new ActiveXObject('WScript.Shell');
+    var steam     = new Steam(fso, shell);
 
-		var path     = steam.getInstallationPath();
-    var userdata = path + '\\userdata';
-    
-		var gameData = steam.getConfig(userdata, '\\7\\remote\\sharedconfig.vdf');
-		var userData = steam.getConfig(userdata, '\\config\\localconfig.vdf');
-		var userID   = steam.getUserID(path,     '\\userid.txt');
-    
-		var gamelist  = [];
-		var imagesize = steam.getSettings('imagesize');
-		steam.loadGames(gamelist, gameData, userData);
+    var path      = steam.getInstallationPath();
+    var userdata  = path + '\\userdata';
+
+    var gameData  = steam.getConfig(userdata, '\\7\\remote\\sharedconfig.vdf');
+    var userID    = steam.getUserID(path,     '\\userid.txt');
+
+    var gamelist  = [];
+    var gamelist2 = [];
+    var imagesize = steam.getSettings('imagesize');
+    var order     = steam.getSettings('order');
+    var nonsteam  = steam.getSettings('nonsteam');
+
+    steam.loadGames(gamelist, gameData);
     gamelist      = steam.convalidateGames(gamelist, userID, imagesize);
-    
+
+    if(nonsteam != 'false') {
+      var shortcuts = steam.getFile(path + '\\shortcuts.xml');
+      if(shortcuts != '')
+        steam.loadNonSteamGames(gamelist2, shortcuts);
+    }
+
     if(gamelist.length == 0) {
-      $('body').css('height', '500px')
-      $('#bg').text('Your Steam gamelist is empty.');
+      $('body') .css('height', '500px')
+      $('#bg')  .text('Your Steam gamelist is empty.');
     }
     else {
-			$('body').css('height', (screen.height - 50) + 'px');
-      steam.printGameList(gamelist, imagesize);
+      $('body') .css('height', (screen.height - 50) + 'px');
+      steam.printGameList(gamelist, imagesize, gamelist2, order);
     }
 
-	}
+  }
   catch(e) {
     $('#bg').text(e.message);
-	}
+  }
 }
--- sources/js/settings.js
+++ sources/js/settings.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 by Giovanni Capuano <webmaster@giovannicapuano.net>
-
+ *
  * Vaporizza is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -14,21 +14,25 @@
  * You should have received a copy of the GNU General Public License
  * along with Vaporizza.  If not, see <http://www.gnu.org/licenses/>.
  */
-function settingsClosing(event) {
-  if(event.closeAction == event.Action.commit)     
+function SettingsClosing(event) {
+  if(event.closeAction == event.Action.commit)
     if(!event.cancel)
       saveSettings();
 }
 
 function saveSettings() {
   System.Gadget.Settings.writeString('imagesize', $('#imagesize').val());
+  System.Gadget.Settings.writeString('order',     $('#order').val());
+  System.Gadget.Settings.writeString('nonsteam',  $('#nonsteam').is(':checked') ? 'true' : 'false');
 }
 
 function getSettings() {
-  $('#imagesize') .val(System.Gadget.Settings.readString('imagesize'));
+  $('#imagesize') .val  (             System.Gadget.Settings.readString('imagesize'));
+  $('#order')     .val  (             System.Gadget.Settings.readString('order'));
+  $('#nonsteam')  .attr ('checked',   System.Gadget.Settings.readString('nonsteam') != 'false');
 }
 
 function init() {
-	System.Gadget.onSettingsClosing = settingsClosing;
+  System.Gadget.onSettingsClosing = SettingsClosing;
   getSettings();
 }
